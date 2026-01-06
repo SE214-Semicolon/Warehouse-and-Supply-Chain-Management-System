dc860f67ae664ac4bf654b166e6ab28596b859a6
AnhQuocHuynh <quocpro12334@gmail.com>
2026-01-06 18:01:43 +0700

    Update sales-order.sanity.spec.ts

--- backend/src/modules/sales/tests/sanity-test/sales-order.sanity.spec.ts
+++ backend/src/modules/sales/tests/sanity-test/sales-order.sanity.spec.ts
@@ -18,7 +18,6 @@ describe('Sales Order Module - Sanity Tests', () => {
   let prisma: PrismaService;
   let jwtService: JwtService;
   let adminToken: string;
-  let adminUserId: string;
   let customerId: string;
 
   beforeAll(async () => {
